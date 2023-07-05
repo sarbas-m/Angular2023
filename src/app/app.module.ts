0d74450bc68492c0c39d9da3fa689d8823d18c3b
sarbas-m <sarbas1996@gmail.com>
2023-07-05 11:25:00 +0530

    sarbas commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ReceptionistComponent } from './receptionist/receptionist.component';
+import { PharmacistComponent } from './pharmacist/pharmacist.component';
+import { LabtechnicianComponent } from './labtechnician/labtechnician.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorComponent,
+    ReceptionistComponent,
+    PharmacistComponent,
+    LabtechnicianComponent
   ],
   imports: [
     BrowserModule,
